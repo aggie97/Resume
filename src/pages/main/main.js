205087c74dff17b74ca82149df6d545ece889ff4
aggie97 <4ggie1997@gmail.com>
2023-04-26 23:37:05 +0900

    :bug: [CI]: fix routes
    fix router path to contain PUBLIC_URL

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -17,8 +17,8 @@ const Main = ({ isnavmode: isNavMode, setIsNavMode }) => {
           <br /> 프론트 엔드 개발자 김민겸입니다.
         </Intro>
         <Nav>
-          <LinkTo to="/resume">Resume</LinkTo>
-          <LinkTo to="/portfolio">Portfolio</LinkTo>
+          <LinkTo to={`${process.env.PUBLIC_URL}/resume`}>Resume</LinkTo>
+          <LinkTo to={`${process.env.PUBLIC_URL}/portfolio`}>Portfolio</LinkTo>
         </Nav>
         <Footer>
           <Link to="https://velog.io/@4ggie97" title="Blog" target="_blank">
